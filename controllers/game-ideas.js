f4bde0e140201e6eaa02c715250f946cb03028a6
jtraya <jtraya7@yahoo.com>
2024-06-11 12:17:55 -0500

    finished create and modified index to allow post to

--- controllers/game-ideas.js
+++ controllers/game-ideas.js
@@ -6,6 +6,44 @@ const GameIdea = require('../models/gameidea')
 router.get('/', async function(req, res){
 
     try {
+        
+        const populatedGameIdeas = await GameIdea.find({}).populate('creator');
+
+        res.render('game-ideas/index.ejs', {
+            gameDocs: populatedGameIdeas,
+        });
+
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+        
+    }
+
+})
+
+router.post('/', async function(req, res){
+
+    try {
+        
+        req.body.creator = req.session.user._id;
+        await GameIdea.create(req.body);
+        res.redirect('/game-ideas');
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+        
+    }
+})
+
+// localhost:3000/game-ideas/new.ejs
+router.get('/new', async function(req, res){
+    res.render('game-ideas/new.ejs')
+})
+
+// localhost:3000/game-ideas
+router.get('/', async function(req, res){
+
+    try {
 
         res.render('game-ideas/index.ejs', {
             user: req.session.user,
